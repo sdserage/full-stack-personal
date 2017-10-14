1849083aa4ec05bd68278a36105b14f1d812af92
Scott Serage <xttocsurge51@gmail.com>
2017-10-13 18:40:08 -0600

    Created a simple sub-menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,11 +2,26 @@ import React, {Component} from 'react';
 import './Header.css';
 import {Link} from 'react-router-dom';
 import {connect} from 'react-redux';
+import {toggleUserMenuOn, toggleUserMenuOff} from '../../ducks/users_reducer';
 
 class Header extends Component{
 
+  constructor(props){
+    super(props);
+    this.toggleUserMenu = this.toggleUserMenu.bind(this);
+  }
+
+  toggleUserMenu(){
+    alert('test');
+  }
+
   render(){
     let user = this.props.user;
+    const {
+      toggleUserMenuOn,
+      toggleUserMenuOff,
+      userMenuDisplayed
+    } = this.props;
     return(
       <header className='header'>
         <nav>
@@ -35,22 +50,47 @@ class Header extends Component{
               <li>Contact</li>
             </Link>
 
-            <Link to='/viewinquiries'>
-              <li className='employeeView'>View Inquiries</li>
-            </Link>
+            {
+              user.isemployee ?
+                /* Renders if the user is an employee */
+                <Link to='/viewinquiries'>
+                  <li className='employeeView'>View Inquiries</li>
+                </Link>
+              :
+                null
+            }
 
           </ul>
           {
             user.userid ?
-              (<div>{user.username}</div>)
+
+              /* Renders when a user is logged in */
+              <div className="user-menu-off" onClick={userMenuDisplayed ? null : toggleUserMenuOn}>
+                {user.username}
+              </div>
+
             :
-            (
+
+              /* Renders when no user is logged in */
+
               <a href={process.env.REACT_APP_LOGIN}>
-                <div>Login/Register</div>
+                <div className="login-or-register">Login/Register</div>
               </a>
-            )
+
           }
         </nav>
+        {
+          userMenuDisplayed ?
+
+            <div className="user-menu-on">
+              <img src='' alt='Close user menu' className='x-icon' onClick={toggleUserMenuOff}/>
+              <a href={process.env.REACT_APP_LOGOUT}>
+                <div>Log Out</div>
+              </a>
+            </div>
+          :
+            null
+        }
       </header>
     );
   }
@@ -59,8 +99,9 @@ class Header extends Component{
 /// This needs to change
 let mapStateToProps = state =>{
   return {
-    user: state.users.user
+    user: state.users.user,
+    userMenuDisplayed: state.users.userMenuDisplayed
   };
 }
 
-export default connect(mapStateToProps)(Header);
+export default connect(mapStateToProps, {toggleUserMenuOn, toggleUserMenuOff})(Header);
--- src/ducks/users_reducer.js
+++ src/ducks/users_reducer.js
@@ -3,11 +3,16 @@ import axios from 'axios';
 let initialState = {
   user: {
     userid: null
-  }
+  },
+  userMenuDisplayed: false
 };
 
+// Action Types
 const GET_USER_INFO = 'GET_USER_INFO';
+const TOGGLE_USER_MENU_ON = 'TOGGLE_USER_MENU_ON';
+const TOGGLE_USER_MENU_OFF = 'TOGGLE_USER_MENU_OFF';
 
+// Axios
 const _FULFILLED = '_FULFILLED';
 
 export function getUserInfo(){
@@ -21,10 +26,31 @@ export function getUserInfo(){
   }
 };
 
+export function toggleUserMenuOn(){
+  return {
+    type: TOGGLE_USER_MENU_ON,
+    payload: true
+  }
+}
+
+export function toggleUserMenuOff(){
+  return {
+    type: TOGGLE_USER_MENU_OFF,
+    payload: false
+  }
+}
+
 export default function users_reducer(state = initialState, action){
   switch(action.type){
     case GET_USER_INFO + _FULFILLED:
       return Object.assign({}, state, {user: action.payload});
+
+    case TOGGLE_USER_MENU_ON:
+      return Object.assign({}, state, {userMenuDisplayed: action.payload});
+
+    case TOGGLE_USER_MENU_OFF:
+      return Object.assign({}, state, {userMenuDisplayed: action.payload});
+
     default:
       return state;
   }
