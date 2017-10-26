1269eea3a11a08bf1284c3570d21433325d2a6aa
Scott Serage <xttocsurge51@gmail.com>
2017-10-25 21:53:11 -0600

    Fixed temperature conversion and storage, still has a slight error when trying to delete all text: it defaults to zero instead of clearing. Main functionality is in tact, moving on

--- src/components/ProductInquiry/itemWizard/ValveWizard/Steps/Step2.js
+++ src/components/ProductInquiry/itemWizard/ValveWizard/Steps/Step2.js
@@ -7,35 +7,64 @@ class Step2 extends Component {
   constructor(props){
     super(props);
     this.state = {
-      celsiusMode: false
+      celsiusMode: false,
+      localTemperature: null
     }
     this.toggleMode = this.toggleMode.bind(this);
+    this.updateTemperatureValue = this.updateTemperatureValue.bind(this);
+    this.convertToCelsius = this.convertToCelsius.bind(this);
+    this.convertToFahrenheit = this.convertToFahrenheit.bind(this);
   }
 
-  toggleMode(){
+  componentDidMount(){
     this.setState({
-      celsiusMode: !this.state.celsiusMode
+      localTemperature: this.props.temperature || null
     });
   }
 
+  toggleMode(bool){
+    if(bool === this.state.celsiusMode){
+      null;
+    }else if(this.state.celsiusMode){
+      this.setState({
+        celsiusMode: !this.state.celsiusMode,
+        localTemperature: this.convertToFahrenheit(this.state.localTemperature)
+      });
+    }else{
+      this.setState({
+        celsiusMode: !this.state.celsiusMode,
+        localTemperature: this.convertToCelsius(this.state.localTemperature)
+      });
+    }
+  }
+
   convertToFahrenheit(celsiusTemp){
-    return (celsiusTemp*1.8+32);
+    return this.round((celsiusTemp*1.8+32), 3);
   }
 
   convertToCelsius(fahrenheitTemp){
-    return (fahrenheitTemp-32)/1.8;
+    return this.round(((fahrenheitTemp-32)/1.8), 3);
   }
-  // isNumberKey(evt){
-  //   var charCode = (evt.which) ? evt.which : event.keyCode
-  //   if (charCode > 31 && (charCode != 46 &&(charCode < 48 || charCode > 57)))
-  //       return false;
-  //   return true;
-  // }
+
+  updateTemperatureValue(temp){
+    this.setState({
+      localTemperature: temp
+    });
+    if(this.state.celsiusMode){
+      temp = this.round(this.convertToFahrenheit(temp), 3);
+    }
+    this.props.setTemperature(temp);
+  }
+
+  round(value, decimals) {
+    return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
+  }
 
   render(){
-    const {celsiusMode} = this.state;
+    const {celsiusMode, celsiusTemp} = this.state;
     const {media, temperature, previousPath, nextPath, setTemperature} = this.props;
     console.log(temperature);
+    console.log(this.state.localTemperature);
     return(
       <div>
         <Link to={previousPath}>
@@ -43,26 +72,17 @@ class Step2 extends Component {
         </Link>
         <h3>What is the temperature of your process?</h3>
         <div>
-          <div onClick={this.toggleMode} className="fahrenheit" style={celsiusMode ? {color: 'black'} : {color: 'red'}}>&#8457;</div>
-          <div onClick={this.toggleMode} className="celsius"style={celsiusMode ? {color: 'red'} : {color: 'black'}}>&#8451;</div>
+          <div onClick={()=>this.toggleMode(false)} className="fahrenheit" style={celsiusMode ? {color: 'black'} : {color: 'red'}}>&#8457;</div>
+          <div onClick={()=>this.toggleMode(true)} className="celsius"style={celsiusMode ? {color: 'red'} : {color: 'black'}}>&#8451;</div>
         </div>
           {
-            celsiusMode ?
-              <div style={{display: 'flex'}}>
-                <input type="number"
-                 value={this.convertToCelsius(temperature)}
-                 onChange={(e)=>setTemperature(this.convertToFahrenheit(Number(e.target.value)))}
-                />
-                <p>&#8451;</p>
-              </div>
-            :
-              <div style={{display: 'flex'}}>
-                <input type="number"
-                 value={temperature}
-                 onChange={(e)=>setTemperature(Number(e.target.value))}
-                />
-                <p>&#8457;</p>
-              </div>
+            <div style={{display: 'flex'}}>
+              <input type="number"
+               value={this.state.localTemperature}
+               onChange={(e) => this.updateTemperatureValue(Number(e.target.value))}
+              />
+              {celsiusMode ? <p>&#8451;</p> : <p>&#8457;</p>}
+            </div>
           }
         {
           typeof temperature === 'number' &&
@@ -79,7 +99,7 @@ const mapStateToProps = state => {
   console.log(state.inquiries.temporaryItem);
   return {
     media: state.inquiries.temporaryItem.media || "Water", // default value for testing, should ultimately be removed
-    temperature: state.inquiries.temporaryItem.temperature || 0
+    temperature: state.inquiries.temporaryItem.temperature
   };
 }
 
