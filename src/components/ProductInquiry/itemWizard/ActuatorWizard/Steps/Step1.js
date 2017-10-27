5ef5014e750b7f15bdb7c2eb2047f36ed5438072
Scott Serage <xttocsurge51@gmail.com>
2017-10-26 22:00:41 -0600

    Started step 1 in actuator wizard

--- src/components/ProductInquiry/itemWizard/ActuatorWizard/Steps/Step1.js
+++ src/components/ProductInquiry/itemWizard/ActuatorWizard/Steps/Step1.js
@@ -4,6 +4,20 @@ import {Link} from 'react-router-dom';
 import {selectMedia} from '../../../../../ducks/inquiries_reducer'
 
 class Step1 extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      localValveSize: null,
+      mmMode: false
+    };
+  }
+
+  componentDidMount(){
+    this.setState({
+      localValveSize: this.props.valvesize || null
+    });
+  }
+
   render(){
     return(
       <div>
@@ -12,4 +26,10 @@ class Step1 extends Component {
   }
 }
 
-export default connect(null)(Step1);
+function mapStateToProps(state){
+  return{
+    valvesize: state.inquiries.temporaryItem.valvesize
+  }
+}
+
+export default connect(mapStateToProps)(Step1);
